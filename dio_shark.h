aefaeb5dd75eefc4aa4e06a22d5ea93eb41558e0
sark <sark@sark-VirtualBox.(none)>
2012-08-16 20:39:59 +0900

    modifying files

--- dio_shark.h
+++ dio_shark.h
@@ -8,15 +8,16 @@
 	in dio-shark, shark means tracing thread
 */
 
+#include <pthread.h>
+#include <stdbool.h>
+#include "dst/dio_list.h"
+
 /* defines */
-//type defines
-#define bool unsigned short
-#define true ~0
-#define false 0
 
 //shark status
 enum shark_stat{
 	SHARK_READY = 0,	//shark thread is ready
+	SHARK_WORKING,
 	SHARK_SICK,		//shark thread has problem
 	SHARK_DONE		//shark thread done all of works
 };
@@ -27,13 +28,17 @@ struct cpu_info{
 
 //shark's personal inventory
 struct shark_inven{
-
+	pthread_t td;
+	pthread_cond_t cond;
+	struct dl_head* list;
+	struct cpu_info cpuinfo;
+	enum shark_stat stat;
 };
 
 /* function declares */
 extern void loose_sharks();			//dealing all sharks(all tracing thread)
-extern bool loose_shark();			//create shark (tracing thread)
-extern void shark_signal();			//for thread synchronizing
+extern bool loose_shark(int no);		//create shark (tracing thread)
+extern void shark_signal(pthread_cond_t cond);			//for thread synchronizing
 extern void wait_allsharks_ready();		//will be called from outer thread
 extern void wait_gunfire();			//shark(tracing thread) will be waiting a start sign
 
